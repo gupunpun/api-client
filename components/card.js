65f846a8f6b9d9536e2333e32fa3e393f84527a6
gupunpun <gubianchi2004@gmail.com>
2024-06-11 21:06:13 -0300

    feat: completo

---
+++ components/card.js
@@ -0,0 +1,22 @@
+export default{
+    name:'Card',
+    props: {
+        film: {
+            type: Object,
+            required: true
+        }
+    },
+    template:` 
+    <div class="card">
+    <img :src="film.movie_banner" height="150" width="150" >
+    <p class="card-title">{{ film.original_title_romanised }}</p>
+    <ul class="card-description">
+      <li>Titulo traduzido: {{ film.title }}</li>
+      <li>Diretor: {{ film.director }}</li>
+      <li>Lançamento: {{ film.release_date }}</li>
+      <li>Score Rotten tomato: {{ film.rt_score }}</li>
+    </ul>
+  </div>
+  `
+
+}
